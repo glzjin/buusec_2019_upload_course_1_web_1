03c0262eb096965dc04c2e1b2ab808c77ae4230d
glzjin <glzjin@zhaojin97.cn>
2019-07-09 12:03:54 +0800

    Add filter

--- files/html/index.php
+++ files/html/index.php
@@ -7,6 +7,15 @@
  */
 
 if(isset($_GET['file'])) {
+    $re = '/^uploads\/[\d|\w]*.jpg$/m';
+    $str = $_GET['file'];
+
+    preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
+
+    if(count($matches) == 0 && $_GET['file'] !== 'upload.php') {
+        die('你不老实哦~');
+    }
+
     include $_GET['file'];
 } else {
     Header('Location: index.php?file=upload.php');
